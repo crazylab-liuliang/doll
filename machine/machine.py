e95415b05387d657345d48d979518554bb50f5d7
albert <qq79402005@gmail.com>
2017-11-01 17:40:57 +0800

    modify

--- machine/machine.py
+++ machine/machine.py
@@ -1,10 +1,19 @@
 import RPi.GPIO as GPIO
+import protocol.pb_machine_control as pb_mc
 
 class dollmachine:
 
 	def __init__(self):
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup( 2, GPIO.OUT)
+
+	def __del__(self):
+		self.cleanup()
+	
+	def on_recv_machine_control(self, msg):
+		if msg.type==1:
+			self.set_forward(msg.op)
+
 
 	def set_forward(self, value):
 		if value!=0:
@@ -13,3 +22,6 @@ class dollmachine:
 		else:
 			print("forward stop")
 			GPIO.output(2, GPIO.LOW)
+
+	def cleanup(self):
+		GPIO.cleanup()
--- machine/main.py
+++ machine/main.py
@@ -2,22 +2,20 @@ import camera
 import network
 import time
 import machine
+import RPi.GPIO as GPIO
 import protocol.pb_machine_control as pb_mc
 import protocol.pb_machine_login as pb_l
 
-# control from player
-def on_recv_machine_control( msg):
-	if msg.type==1:
-		dm.set_forward(msg.op)
+def run():
+	dm = machine.dollmachine()
 
+	#camera.begin_push_video_stream_0()
+	nw = network.network()
+	nw.connect_server("10.237.24.45", 8800)
+	nw.login()
+	nw.bind(pb_mc.machine_control(), dm.on_recv_machine_control)
+	while True:
+		nw.loop()
 
-dm = machine.dollmachine()
-
-#camera.begin_push_video_stream_0()
-nw = network.network()
-nw.connect_server("10.237.24.45", 8800)
-nw.login()
-nw.bind(pb_mc.machine_control(), on_recv_machine_control)
-
-while True:
-	nw.loop()
+# run
+run()
