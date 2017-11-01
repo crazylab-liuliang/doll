dd842ea8a546a3da89e769815e30272f1894e475
Albert Cameron <qq79402005@gmail.com>
2017-11-01 14:37:48 +0800

    删除无用的客户端资源。

--- client/launch/main/main.gd
+++ client/launch/main/main.gd
@@ -1,8 +1,4 @@
 extends Control
 
 func _on_play_pressed():
-	get_node("/root/sound").play_sound("button_press")
-	get_node("/root/global").set_scene("res://game_single/game_single.tscn")
-
-func _on_vs_pressed():
-	get_node("/root/global").set_scene("res://game_single_vs/game_single_vs.tscn")
+	get_node("/root/global").set_scene("res://room/room.tscn")
