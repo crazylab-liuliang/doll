c1d735f8a35e07a255c82dd7a5f91e9d8212b06c
Albert Cameron <qq79402005@gmail.com>
2017-11-04 17:53:44 +0800

    修改为外网服务器

--- client/room/machine_control.gd
+++ client/room/machine_control.gd
@@ -1,31 +1,37 @@
 extends Control
 
+var is_mirror = true
+var button_idxs = []
+
 func _ready():
-	pass
+	if is_mirror:
+		button_idxs = [2,1,4,3]
+	else:
+		button_idxs = [1,2,3,4]
 
 func _on_forward_button_down():
-	get_node("/root/network").send_machine_control(1,1)
+	get_node("/root/network").send_machine_control(button_idxs[0],1)
 
 func _on_forward_button_up():
-	get_node("/root/network").send_machine_control(1,0)
+	get_node("/root/network").send_machine_control(button_idxs[0],0)
 
 func _on_back_button_down():
-	get_node("/root/network").send_machine_control(2,1)
+	get_node("/root/network").send_machine_control(button_idxs[1],1)
 
 func _on_back_button_up():
-	get_node("/root/network").send_machine_control(2,0)
+	get_node("/root/network").send_machine_control(button_idxs[1],0)
 
 func _on_left_button_down():
-	get_node("/root/network").send_machine_control(3,1)
+	get_node("/root/network").send_machine_control(button_idxs[2],1)
 
 func _on_left_button_up():
-	get_node("/root/network").send_machine_control(3,0)
+	get_node("/root/network").send_machine_control(button_idxs[2],0)
 
 func _on_right_button_down():
-	get_node("/root/network").send_machine_control(4,1)
+	get_node("/root/network").send_machine_control(button_idxs[3],1)
 
 func _on_right_button_up():
-	get_node("/root/network").send_machine_control(4,0)
+	get_node("/root/network").send_machine_control(button_idxs[3],0)
 
 func _on_start_pressed():
 	get_node("/root/network").send_machine_control(0,0)
