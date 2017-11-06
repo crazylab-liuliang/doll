73dd9f2b7a20ce726e1a4851f4b84ab6a694946d
Albert Cameron <qq79402005@gmail.com>
2017-11-06 16:51:05 +0800

    http服务

--- machine_http_server/camera.py
+++ machine_http_server/camera.py
@@ -6,7 +6,7 @@ class live_camera:
 	camera_process_0 = None
 
 	def __del__(self):
-		stop_push_video_stream_0()
+		self.stop_push_video_stream_0()
 
 	def begin_push_video_stream_0(self):
 		camera_process_0 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-framerate', '25', '-video_size', '640x480', '-i', '/dev/video0', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', '640x480', '-b:v', '1000k', '-bf', '0', 'http://118.190.156.61:10001/secret'])
