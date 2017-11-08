df3c9c111a3bf96a6ccacabc56974d424f33d7e6
Albert Cameron <qq79402005@gmail.com>
2017-11-08 18:30:35 +0800

    摄像头分辨率

--- machine_http_server/camera.py
+++ machine_http_server/camera.py
@@ -10,7 +10,7 @@ class live_camera:
 		self.stop_push_video_stream_0()
 
 	def begin_push_video_stream_0(self):
-		camera_process_0 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-framerate', '25', '-video_size', '480x640', '-i', '/dev/video0', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', '640x480', '-b:v', '1000k', '-bf', '0', 'http://118.190.156.61:10001/camera_0'])
+		camera_process_0 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-framerate', '25', '-video_size', '480x640', '-i', '/dev/video0', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', '480x640', '-b:v', '1000k', '-bf', '0', 'http://118.190.156.61:10001/camera_0'])
 		print("begin push video stream 0")
 
 	def stop_push_video_stream_0(self):
@@ -18,7 +18,7 @@ class live_camera:
 			camera_process_0.terminate()
 
 	def begin_push_video_stream_1(self):
-		camera_process_1 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-framerate', '25', '-video_size', '480x640', '-i', '/dev/video1', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', '640x480', '-b:v', '1000k', '-bf', '0', 'http://118.190.156.61:10001/camera_1'])
+		camera_process_1 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-framerate', '25', '-video_size', '480x640', '-i', '/dev/video1', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', '480x640', '-b:v', '1000k', '-bf', '0', 'http://118.190.156.61:10001/camera_1'])
 		print("begin push video stream 1")
 
 	def stop_push_video_stream_1(self):
