2da93705b18dba3e3965cb2dec9df6638daac104
qq79402005@gmail.com <qq79402005@gmail.com>
2017-11-30 00:02:56 +0800

    更改推流视频大小

--- machine/camera.py
+++ machine/camera.py
@@ -7,20 +7,21 @@ class live_camera:
 	camera_process_1 = None
 
 	def __del__(self):
-		stop_push_video_stream_0()
+		self.stop_push_video_stream_0()
+		self.stop_push_video_stream_1()
 
 	def begin_push_video_stream_0(self):
-		camera_process_0 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-framerate', '25', '-video_size', '640x480', '-i', '/dev/video0', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', '640x480', '-b:v', '1000k', '-bf', '0', 'http://118.190.156.61:10001/camera_0'])
+		self.camera_process_0 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-framerate', '25', '-video_size', '320x240', '-i', '/dev/video0', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', '320x240', '-b:v', '1000k', '-bf', '0', 'http://118.190.156.61:10001/camera_0'])
 		print("begin push video stream 0")
 
 	def stop_push_video_stream_0(self):
-		if camera_process_0!=None:
-			camera_process_0.terminate()
+		if self.camera_process_0!=None:
+			self.camera_process_0.terminate()
 
 	def begin_push_video_stream_1(self):
-		camera_process_1 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-framerate', '25', '-video_size', '640x480', '-i', '/dev/video1', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', '640x480', '-b:v', '1000k', '-bf', '0', 'http://118.190.156.61:10001/camera_1'])
+		self.camera_process_1 = subprocess.Popen(['ffmpeg', '-f', 'v4l2', '-framerate', '25', '-video_size', '320x240', '-i', '/dev/video1', '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', '320x240', '-b:v', '1000k', '-bf', '0', 'http://118.190.156.61:10001/camera_1'])
 		print("begin push video stream 1")
 
 	def stop_push_video_stream_1(self):
-		if camera_process_1!=None:
-			camera_process_1.terminate()
+		if self.camera_process_1!=None:
+			self.camera_process_1.terminate()
