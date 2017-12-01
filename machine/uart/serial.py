b3d7c24a292683068cc312cffb77cf1133c735b8
Albert Cameron <qq79402005@gmail.com>
2017-12-01 17:23:47 +0800

    串口python 代码提交。

---
+++ machine/uart/serial.py
@@ -0,0 +1,19 @@
+import serial
+
+class uart:
+    ser = None
+
+    def __init__(self):
+        self.ser = serial.Serial("/dev/ttyAMA0")
+        self.ser.baudrate =
+
+    def __del__(self):
+        self.ser.close()
+
+    def write(data):
+        self.ser.write(data)
+        return
+
+    def receive(self, num):
+        data = self.ser.read(num)
+        return data
